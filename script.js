10174fd03ca17f77a2c445c8df7b806220c418b7
chloelsmith <143556876+chloelsmith@users.noreply.github.com>
2024-04-02 23:50:00 -0500

    javascript function fron API documentation

--- script.js
+++ script.js
@@ -1,8 +1,86 @@
 
-// step 1 make a form 
+function getAndDisplayToDos(){
 
-documnet.getElementById("ToDo").addEventListener("Add", addToDo );
+    let xhttp = new XMLHttpRequest();
 
-function addToDo(event){
-    // event.preventDefault();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let todos = JSON.parse(this.responseText);
+            console.log(todos);
+            displayToDos(todos);
+        }
+    };
+
+    xhttp.open("GET", "https://cse204.work/todos", true);
+    xhttp.setRequestHeader("x-api-key","93064f-f9b3f4-8689e1-3a0fa1-e3a22b");
+    xhttp.send();
 }
+
+function displayToDos(todos){
+
+
+}
+
+window.onload = function(){
+    getAndDisplayToDos();
+}
+
+function addNewTodo(){
+    let data = {
+        text: "Some Text"
+    }
+    let xhttp2 = new XMLHttpRequest();
+    xhttp2.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let todo = JSON.parse(this.responseText);
+            console.log(todo);
+        } else if (this.readyState == 4) {
+            console.log(this.responseText);
+
+        }
+    };
+    xhttp2.open("POST", "https://cse204.work/todos", true);
+    xhttp2.setRequestHeader("Content-type", "application/json");
+    xhttp2.setRequestHeader("x-api-key", "93064f-f9b3f4-8689e1-3a0fa1-e3a22b");
+    xhttp2.send(JSON.stringify(data))
+}
+
+
+
+function updateTodo(id, status){
+    let xhttp = newXMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.state == 4){
+            if (this.status ==200){
+                console.log('Todo updated');
+            } else{
+                console.error('Failed to update ToDo', this.responseText);
+            }
+        }
+    };
+    xhttp.open('PUT', `https://cse204.work/todos/${id}`, true);
+    xhttp2.setRequestHeader("Content-type", "application/json");
+    xhttp2.setRequestHeader("x-api-key", "93064f-f9b3f4-8689e1-3a0fa1-e3a22b");
+    xhttp.send(JSON.stringify(newData));
+}
+
+function deleteTodo(id){
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                console.log('ToDo deleted successfully');
+            } else {
+                console.error('Failed to delete ToDo:', this.responseText);
+            }
+        }
+    };
+
+    xhttp.open('DELETE', `https://cse204.work/todos/${id}`, true);
+    xhttp.setRequestHeader('Content-type', 'application/json');
+    xhttp.setRequestHeader('x-api-key', '93064f-f9b3f4-8689e1-3a0fa1-e3a22b'); 
+    xhttp.send();
+}
+
